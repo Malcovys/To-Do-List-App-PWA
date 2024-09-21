0d1e243ead57cee0c39bde87dd38ee652b9539fb
malcovys <malcovys22.aps@gmail.com>
2024-09-21 15:10:06 +0300

    create task modal

--- remind/src/App.tsx
+++ remind/src/App.tsx
@@ -9,22 +9,16 @@ export interface Task {
 };
 
 function App() {
-  //const [tasks, setTask] = useState<Task[]>([]);
+  const [tasks, setTask] = useState<Task[]>([]);
 
-  const tasks = [
-    { id: 1, title: "Learn TypeScript", completed: false },
-    { id: 2, title: "Build a React app", completed: true },
-  ];
-
-  const onSubmit = (input : string) => {
+  const hadleNewTask = (input : string) => {
       let newTask : Task = {
           id: -1,
           title : input,
           completed: false
       };
-
-      // setTask([...tasks, newTask]);
-      // console.log(tasks);
+      setTask([...tasks, newTask]);
+      console.log(tasks);
   }
 
   return (
@@ -34,7 +28,7 @@ function App() {
         <TaskList tasks={tasks} />
       </div>
       <div className="absolute bottom-5 right-7">
-        <TaskCreator/>
+        <TaskCreator submitCallback={hadleNewTask} />
       </div>
     </div>
   )
--- remind/src/components/personal/TaskCreator.tsx
+++ remind/src/components/personal/TaskCreator.tsx
@@ -1,41 +1,75 @@
-import { Modal, ModalBody, ModalContent, ModalFooter, ModalTrigger } from "../ui/animated-modal";
+import { useEffect, useState } from "react";
+import { Modal, ModalBody, ModalContent, ModalFooter, ModalProvider, ModalTrigger } from "../ui/animated-modal";
 
-const TaskCreator: React.FC = () => {
+const TaskCreator: React.FC<{submitCallback:(input : string) => void | null }> = ({submitCallback}) => {
   /* Component states and datas */
+  const minInputCharater = 2;
+  const [inputValue, setInputValue] = useState(""); 
+  const [validableInput, setValidableInput] = useState(false);
+  const submitButtonStyle = {
+    "unSubmitable" : "bg-black text-white text-sm px-2 py-1 rounded-md w-28 bg-opacity-10",
+    "submitable" : "bg-black text-white text-sm px-2 py-1 rounded-md w-28 hover:bg-opacity-75"
+  }
 
   /* Component behaviors */
+  const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if(!validableInput) return;
+
+    if(submitCallback != null) submitCallback(inputValue);
+    
+    setInputValue("");
+  }
+
+  const handleInputChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+    console.log(inputValue.trim().length);
+  }
+
+  useEffect(() => {
+    if(inputValue.trim().length >= minInputCharater) {
+      setValidableInput(true);
+      return;
+    } 
+    setValidableInput(false);
+  }, [inputValue])
 
   /* Component render */
   return (
-    <div>
-      <Modal>
-        <ModalTrigger className="bg-black text-white rounded-lg w-11 h-11 flex justify-center items-center">
+    <form onSubmit={handleSubmit}>
+      <ModalProvider>
+        <Modal>
+          <ModalTrigger className="bg-black text-white rounded-lg w-11 h-11 flex justify-center items-center">
             <span className="text-center text-3xl">+</span>
-        </ModalTrigger>
-        <ModalBody className="mx-2">
+          </ModalTrigger>
+          <ModalBody className="mx-2">
             <ModalContent>
-                <h4 className="text-lg md:text-2xl text-neutral-600 font-bold text-center mb-8">
-                    Create new{" "}
-                    <span className="px-1 py-0.5 rounded-md bg-gray-100 border-gray-200">
-                        Task
-                    </span>{" "}
-                    now! ✈️
-                </h4>
-                <label htmlFor="task-input">To-do</label>
-                <input className="h-10 border-l-2"
-                    type="text" 
-                    name="task-input" 
-                    id="task-input" 
-                />
+              <h4 className="text-lg md:text-2xl text-neutral-600 font-bold text-center mb-8">
+                Create new{" "}
+                <span className="px-1 py-0.5 rounded-md bg-gray-100 border-gray-200">
+                  Task
+                </span>{" "}
+                now! ✈️
+              </h4>
+              <label htmlFor="task-input" className="text-slate-500">To-do</label>
+              <input className="h-10 border-l-2 font-medium"
+                  type="text" 
+                  name="task-input" 
+                  id="task-input"
+                  value={inputValue}
+                  onChange={handleInputChange}
+              />
             </ModalContent>
             <ModalFooter className="gap-4">
-                <button className="bg-black text-white text-sm px-2 py-1 rounded-md border border-black w-28">
-                    Create
-                </button>
+              <button className={validableInput ? submitButtonStyle.submitable : submitButtonStyle.unSubmitable}>
+                Create
+              </button>
             </ModalFooter>
-        </ModalBody>
-      </Modal>
-    </div>
+          </ModalBody>
+        </Modal>
+      </ModalProvider>
+    </form>
   );
 };
 
