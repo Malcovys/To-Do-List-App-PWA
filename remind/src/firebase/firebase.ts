77ec4ebdeb03a633a86ffc81e03dda779cf7bf69
malcovys <malcovys22.aps@gmail.com>
2024-09-24 11:27:09 +0300

    clear packages

---
+++ remind/src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCN3JmbOWozCH3c9fihNvQebwLfto0cfJM",
+  authDomain: "remind-d9d5f.firebaseapp.com",
+  projectId: "remind-d9d5f",
+  storageBucket: "remind-d9d5f.appspot.com",
+  messagingSenderId: "854741784842",
+  appId: "1:854741784842:web:af8123ae6546697c3d02f3",
+  measurementId: "G-DZC50REEE9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
